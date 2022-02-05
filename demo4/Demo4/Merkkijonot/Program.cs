b4477b0a4f1c259efdee756274af30b6f334161b
Eemil Kauppinen <kauppinen.eemil@gmail.com>
2022-02-05 12:55:51 +0200

    Demo 4 lisatty.

---
+++ demo4/Demo4/Merkkijonot/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Merkkijonot
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Anna 1. jono >");
+        string jono1 = Console.ReadLine();
+        Console.Write("Anna 2. jono >");
+        string jono2 = Console.ReadLine();
+        string pidempi = PidempiMerkkijono(jono1, jono2);
+        Console.WriteLine("\"" + pidempi + "\" on pidempi merkkijono");
+    }
+    /// <summary>
+    /// Funktio, joka ottaa vastaan kaksi merkkijonoa, ja palauttaa näistä kahdesta pidemmän merkkijonon. Jonojen ollessa yhtä pitkiä palautetaan ensimmäisenä parametrina annettu jono.
+    /// </summary>
+    /// <param name="jono1">merkkijono1</param>
+    /// <param name="jono2">merkkijono2</param>
+    /// <returns>Palauttaa pidemmän merkkijonon tai jonojen ollessa yhtä pitkiä paluttaa ekan</returns>
+    public static string PidempiMerkkijono(string jono1, string jono2)
+    {
+        if (jono1.Length > jono2.Length )
+        {
+            return jono1;
+        }
+        else if (jono1.Length < jono2.Length)
+        {
+            return jono2;
+        }
+        else 
+        {
+            return jono1;
+        }
+    }
+}
---
+++ demo4/Demo4/Suorakulmiot/Suorakulmiot.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+/// @author  Vesa Lappalainen, Antti-Jussi Lakanen
+/// @version 2022
+///
+/// <summary>
+/// Ohjelmassa pudotellaan suorakulmioita lattialle.
+/// Harjoitellaan fysiikan käyttöä, toistolausetta (for),
+/// sekä aliohjelmien luomista.
+/// </summary>
+public class Suorakulmiot : PhysicsGame
+{
+    public override void Begin()
+    {
+        Level.Size = new Vector(1300, 700); // asettaa pelialueen koon
+        SetWindowSize(1300, 700); // asettaa ikkunan koon
+        Level.CreateBorders(); // luodaan kentälle reunat
+        Camera.ZoomToLevel(); // zoomaa kameran siten että vain pelialue on näkyvillä, eikä muuta
+
+        Level.Background.Color = Color.Black;
+        //Gravity = new Vector(0, -1000);
+
+        for (int i = 0; i < 30; i++)
+        {
+            double sade = RandomGen.NextDouble(5, 30);
+            PiirraSuorakulmio(
+                this,
+                RandomGen.NextDouble(Level.Left, Level.Right), // Kentän vasen ja oikea reuna
+                RandomGen.NextDouble(Level.Bottom, Level.Top), // alareuna, yläreuna
+                sade, // leveys
+                sade // korkeus
+                );
+        }
+        PiirraSuorakulmio(this, 0, 0, 40, 40, Color.White);
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    /// <summary>
+    /// Luodaan ja lisätään ruudulle annetun kokoinen
+    /// suorakulmio annettuun paikkaan.
+    /// </summary>
+    /// <param name="peli">Peli, johon neliö piirretään</param>
+    /// <param name="x">Kappaleen keskipisteen x-koordinaatti</param>
+    /// <param name="y">Kappaleen keskipisteen y-koordinaatti</param>
+    /// <param name="w">Kappaleen leveys.</param>
+    /// <param name="h">Kappaleen korkeus.</param>
+    public static void PiirraSuorakulmio(Game peli, double x, double y, double w, double h)
+    {
+        PhysicsObject kappale = new PhysicsObject(w, h, Shape.Circle);
+        kappale.Position = new Vector(x, y);
+        kappale.Color = RandomGen.NextColor();
+        peli.Add(kappale);
+    }
+
+    public static void PiirraSuorakulmio(Game peli, double x, double y, double w, double h, Color color)
+    {
+        PhysicsObject kappale = new PhysicsObject(w, h, Shape.Circle);
+        kappale.Position = new Vector(x, y);
+        kappale.Color = color;
+        peli.Add(kappale);
+    }
+}
